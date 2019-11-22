b59f2c3b927b78fe8bc43b6cde636bc27fb1fbce
frank <frank.bommeli@mimacom.com>
2019-11-21 17:15:39 -0700

    Display manifest finally

--- src/main/kotlin/org/fb/kdiff/ui/KdiffController.kt
+++ src/main/kotlin/org/fb/kdiff/ui/KdiffController.kt
@@ -2,9 +2,13 @@ package org.fb.kdiff.ui
 
 import javafx.fxml.FXML
 import javafx.scene.control.Label
-import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
+import java.io.InputStream
+import java.net.URL
+import java.net.URLConnection
+import java.util.*
 import java.util.jar.Manifest
+
 
 @Component
 class KdiffController {
@@ -14,9 +18,36 @@ class KdiffController {
 
     @FXML
     fun initialize() {
-//        val r = ClassPathResource("/META-INF/frbo.mf")
-        val r = ClassPathResource("/META-INF/MANIFEST.MF")
-        val m = Manifest(r.inputStream)
+        val resources: Enumeration<URL> = Thread.currentThread().contextClassLoader
+                .getResources("META-INF/MANIFEST.MF")
+
+
+        val protectionDomain = this::javaClass.javaClass.protectionDomain
+        val codeSource = protectionDomain.codeSource
+        val location = codeSource.location
+        val path = location.path
+        println("path $path")
+        var sub = path.substring(1, path.indexOf(".jar") + 4)
+        println("sub $sub")
+
+        sub = sub.substring(sub.lastIndexOf("/") + 1)
+        println("sub $sub")
+
+        val other = "${sub}!/META-INF/MANIFEST.MF"
+        println(other)
+        val rls = mutableListOf<URL>()
+        for (rr in resources) {
+            if (rr.path.contains(other))
+                rls.add(rr)
+        }
+
+        for (rl in rls)
+            println(rl)
+
+        val urlConnection: URLConnection = rls[0].openConnection()
+        val iss: InputStream = urlConnection.getInputStream()
+
+        val m = Manifest(iss)
 
         manifest.text = m.mainAttributes
                 .map { (key, value) -> "${key}:\t$value" }
