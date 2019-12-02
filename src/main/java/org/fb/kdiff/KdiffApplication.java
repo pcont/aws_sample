d576bad7cd6b450be2f4b5bfa2e5a1f0e42e8f01
frank <frank.bommeli@mimacom.com>
2019-12-02 16:34:02 -0700

    TAKEDA-1227 : removing kotlin

---
+++ src/main/java/org/fb/kdiff/KdiffApplication.java
@@ -0,0 +1,27 @@
+package org.fb.kdiff;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class KdiffApplication extends Application {
+
+    private ConfigurableApplicationContext context;
+
+    public void init() {
+        context = new SpringApplicationBuilder()
+                .sources(Main.class)
+                .run();
+    }
+
+    public void start(Stage primaryStage) {
+        context.publishEvent(new StageReadyEvent(primaryStage));
+    }
+
+    public void stop() {
+        context.close();
+        Platform.exit();
+    }
+}
---
+++ src/main/java/org/fb/kdiff/Main.java
@@ -0,0 +1,11 @@
+package org.fb.kdiff;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Main {
+    public static void main(String[] args) {
+        Application.launch(KdiffApplication.class, args);
+    }
+}
