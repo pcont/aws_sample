ec53a9ee82f711944f51bf75eddd31f2c93591ee
frank <frank.bommeli@mimacom.com>
2019-12-09 11:36:22 -0700

    TAKEDA-1390 : using junit 4 instead of 5

--- src/test/java/org/fb/manifest/api/ManifestServiceIT.java
+++ src/test/java/org/fb/manifest/api/ManifestServiceIT.java
@@ -1,10 +1,11 @@
 package org.fb.manifest.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertNotNull;
+
 
 @SpringBootTest
 class ManifestServiceIT {
